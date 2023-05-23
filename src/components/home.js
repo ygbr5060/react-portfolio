2e6a5b1cd930d38d94836f87619efd18680b4a3a
ygbr5060 <aqibkhan5060@gmail.com>
2023-05-23 22:14:41 +0100

    intial home.js

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to My Portfolio</h2>
+      <h1>Aqib khan</h1>
+      <img src="/" alt="profile picture" />
+      <p>welcome to my portfolio</p>
+    </div>
+  );
+};
+
+export default Home;
